c9fd54cba0bbab5d2b3bc71259189655f86e0ec5
AISHANGQIAN <431942323@qq.com>
2019-02-13 15:39:07 +0800

    网络请求数据

---
+++ lx_one/src/main/java/com/example/lx_one/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.lx_one;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String path = "http://result.eolinker.com/iYXEPGn4e9c6dafce6e5cdd23287d2bb136ee7e9194d3e9?uri=one";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String data = getData();
+                Log.e("myMessage",""+data);
+            }
+        }).start();
+
+
+
+    }
+
+    public String getData(){
+
+        try {
+            URL url = new URL(path);
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            if (urlConnection.getResponseCode()==200){
+                InputStream inputStream = urlConnection.getInputStream();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                byte[] bytes = new byte[1024];
+
+                int length = 0;
+                while ((length = inputStream.read(bytes))!= -1){
+                    byteArrayOutputStream.write(bytes,0, length);
+                    Log.e("aaaa",""+length);
+                }
+                String value = byteArrayOutputStream.toString();
+
+                return value;
+            }
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+}
